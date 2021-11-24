e7618f27d93bccf8bea228fbc0539efef7aaf244
KirillMakrutin <kirill.makrutin@ideas.com>
2021-11-24 20:32:06 +0300

    Axon config

--- user-cmd-api/src/main/java/com/kmakrutin/user/cmd/api/UserCmdApiApplication.java
+++ user-cmd-api/src/main/java/com/kmakrutin/user/cmd/api/UserCmdApiApplication.java
@@ -2,8 +2,11 @@ package com.kmakrutin.user.cmd.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+import com.kmakrutin.user.core.configuration.AxonConfig;
 
 @SpringBootApplication
+@Import(AxonConfig.class)
 public class UserCmdApiApplication {
 
     public static void main(String[] args) {
--- user-query-api/src/test/java/com/kmakrutin/user/query/api/UserQueryApiApplicationTests.java
+++ user-query-api/src/test/java/com/kmakrutin/user/query/api/UserQueryApiApplicationTests.java
@@ -1,8 +1,10 @@
 package com.kmakrutin.user.query.api;
 
+import com.kmakrutin.user.core.configuration.AxonConfig;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
 @SpringBootTest
 class UserQueryApiApplicationTests {
