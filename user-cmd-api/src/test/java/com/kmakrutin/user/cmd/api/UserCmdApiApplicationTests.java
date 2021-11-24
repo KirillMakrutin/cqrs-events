eae17addaf568b5d5a503593cd32a13768311e80
KirillMakrutin <kirill.makrutin@ideas.com>
2021-11-24 18:05:58 +0300

    User management project

---
+++ user-cmd-api/src/test/java/com/kmakrutin/user/cmd/api/UserCmdApiApplicationTests.java
@@ -0,0 +1,15 @@
+package com.kmakrutin.user.cmd.api;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserCmdApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+        Assertions.assertTrue(true);
+    }
+
+}
---
+++ user-query-api/src/test/java/com/kmakrutin/user/query/api/UserQueryApiApplicationTests.java
@@ -0,0 +1,15 @@
+package com.kmakrutin.user.query.api;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserQueryApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+        Assertions.assertTrue(true);
+    }
+
+}
